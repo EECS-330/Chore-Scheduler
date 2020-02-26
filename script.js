fda2308375b3eb1007b34f47f0d3399fc2e569c6
Anthony <chen.j.anthony@gmail.com>
2020-02-25 21:55:59 -0600

    prototype 2

--- script.js
+++ script.js
@@ -6,38 +6,78 @@ function greenify(checkName)
 document.getElementById(checkName).parentElement.style.background = '#9fdf9f';
   } else {
 document.getElementById(checkName).parentElement.style.background = '#ffcccc';
-  }
-   
-   
+  }  
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
+var people = [{"id":0,"name":"User0", "people":["John Smith", "Bob Jones", "Sam Johnson"]},{"id":1,"name":"User1", "people": ["yes", "no", "maybe"]}]
+displayPeople(people)
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// Get the task modal
+var modal_task = document.getElementById("myModal_task");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the person modal
+var modal_person = document.getElementById("myModal_person");
 
-var submit = document.getElementById("submitBtn");
+// Get the task button that opens the task modal
+var btn_task = document.getElementById("myBtn_task");
+
+// Get the person button that opens the person modal
+var btn_person = document.getElementById("myBtn_person");
+
+
+// Get the <span> element that closes the person modal
+var span_person = document.getElementsByClassName("close")[1];
+
+// Get the <span> element that closes the task modal
+var span_task = document.getElementsByClassName("close")[0];
+
+var submit_task = document.getElementById("submitBtn_task");
 // When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+btn_task.onclick = function() {
+    modal_task.style.display = "block";
+}
+
+// When the user clicks the button, open the modal
+btn_person.onclick = function() {
+    modal_person.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+span_task.onclick = function() {
+    modal_task.style.display = "none";
 }
 
-submit.onclick = function() {
-    modal.style.display = "none";
+// When the user clicks on <span> (x), close the modal
+span_person.onclick = function() {
+    modal_person.style.display = "none";
+}
+
+submit_task.onclick = function() {
+    modal_task.style.display = "none";
+}
+
+function displayPeople(json){
+    const element = document.getElementById("people");
+    var user = 0;
+    var names = json[user].people;
+    for (var i = 0; i < names.length; i++) {
+        const person = document.createElement("section");
+        person.setAttribute("class", "person-card")
+        person.innerHTML = `
+           <h3 class="person-name">${names[i]}</h3>
+           <img class="person-icon" src="https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png">
+        `
+        element.appendChild(person)
+    }
+    console.log("hey")
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal_task) {
+        modal_task.style.display = "none";
+    }
+    else if (event.target == modal_person) {
+        modal_person.style.display = "none";
     }
 }
