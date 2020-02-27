5bbf1c58404bd2dc732ebd9d99832330d74ac1d3
Anthony <chen.j.anthony@gmail.com>
2020-02-26 20:44:16 -0600

    made tasks dynamic

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ document.getElementById(checkName).parentElement.style.background = '#9fdf9f';
 document.getElementById(checkName).parentElement.style.background = '#ffcccc';
   }  
 }
-
-var people = [{"id":0,"name":"User0", "people":["John Smith", "Bob Jones", "Sam Johnson"]},{"id":1,"name":"User1", "people": ["yes", "no", "maybe"]}]
+var test = [1,2,3]
+var people = [{"id":0,"name":"User0", "people":["John Smith", "Bob Jones", "Sam Johnson"]},{"id":1,"name":"User1", "people": ["William", "Jeb", "Emily", "Alice"]}]
+var tasks = [{"id":0,"tasks":[["Trash", "Throwing out trash and replacing garbage bag", "10 minutes"], ["Clean", "Vaccum room and mop floor", "45 minutes"]]},
+    {"id":1,"tasks":[["Errand", "Buy groceries", "60 minutes"],["Clean", "cleaning bathroom", "30 minutes"], ["Errand", "Buy toilet paper", "15 minutes"]]}]
 
 // Get the task modal
 var modal_task = document.getElementById("myModal_task");
@@ -64,9 +66,27 @@ function displayPeople(json){
         `
         element.appendChild(person)
     }
-    console.log("hey")
 }
 
+function displayTasks(json) {
+    const element = document.getElementById("tasks");
+    var user = window.location.hash.substr(1);
+    var tasks = json[user].tasks
+    for (var i = 0; i < tasks.length; i++) {
+        const task = tasks[i];
+        const thing = document.createElement("section");
+        thing.setAttribute("class", "task-card");
+        const taskid = "completed" + i;
+        thing.innerHTML = `
+            <h3 class="task-name">${task[0]}</h3>
+            <p class="task-description">${task[1]}</p>
+            <p class="task-duration">${task[2]}</p>
+            <input type="checkbox" id=${taskid} class="task-completion" value="vachinde" onclick="greenify(this.id);">
+            <br>
+        `
+        element.appendChild(thing)
+    }
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal_task) {
